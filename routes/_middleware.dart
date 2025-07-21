c92af084c16636ac739f5d04aa8e63d4e31a5f1c
lucas-goldner <lucas.goldner@googlemail.com>
2025-07-22 00:04:10 +0900

    chore: Support cors

--- routes/_middleware.dart
+++ routes/_middleware.dart
@@ -1,4 +1,5 @@
 import 'package:dart_frog/dart_frog.dart';
+import 'package:shelf_cors_headers/shelf_cors_headers.dart' as shelf;
 
 import '../middleware/integration_test_preference_provider.dart';
 import '../middleware/test_types_provider.dart';
@@ -7,4 +8,15 @@ import '../middleware/testing_question_provider.dart';
 Handler middleware(Handler handler) => handler
     .use(testingQuestionProvider)
     .use(testTypesProvider)
-    .use(integrationTestPreferenceProvider);
+    .use(integrationTestPreferenceProvider)
+    .use(requestLogger())
+    .use(
+      fromShelfMiddleware(
+        shelf.corsHeaders(
+          headers: {
+            shelf.ACCESS_CONTROL_ALLOW_ORIGIN:
+                'https://master-presentation-questions.globeapp.dev/',
+          },
+        ),
+      ),
+    );
